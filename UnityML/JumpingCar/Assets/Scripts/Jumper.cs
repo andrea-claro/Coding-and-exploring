2fde56a23772181e86503be5c9ee219e42455ce9
Andrea <55531272+andrea-claro@users.noreply.github.com>
2020-11-26 09:57:25 +0100

    Adding Project

---
+++ UnityML/JumpingCar/Assets/Scripts/Jumper.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using UnityEngine;
+
+public class Jumper : Agent
+{
+    [SerializeField] private float jumpForce;
+    [SerializeField] private KeyCode jumpKey;
+    
+    private bool jumpIsReady = true;
+    private Rigidbody rBody;
+    private Vector3 startingPosition;
+    private int score = 0;
+    
+    public event Action OnReset;
+
+    public override void Initialize()
+    {
+        rBody = GetComponent<Rigidbody>();
+        startingPosition = transform.position;
+    }
+
+    public override void OnActionReceived(float[] vectorAction)
+    {
+        if (Mathf.FloorToInt(vectorAction[0]) == 1)
+            Jump();
+    }
+
+    public override void OnEpisodeBegin()
+    {
+        Reset();
+    }
+
+    public override void Heuristic(float[] actionsOut)
+    {
+        actionsOut[0] = 0;
+
+        if (Input.GetKey(jumpKey))
+            actionsOut[0] = 1;
+    }
+
+    public void FixedUpdate()
+    {
+        if (jumpIsReady)
+            RequestDecision();
+    }
+
+    private void Jump()
+    {
+        if (jumpIsReady)
+        {
+            rBody.AddForce(new Vector3(0, jumpForce, 0), ForceMode.VelocityChange);
+            jumpIsReady = false;
+        }
+    }
+
+    private void Reset()
+    {
+        score = 0;
+        jumpIsReady = true;
+        
+        //Reset Movement and Position
+        transform.position = startingPosition;
+        rBody.velocity = Vector3.zero;
+        
+        OnReset?.Invoke();
+    }
+
+    private void OnCollisionEnter(Collision collidedObj)
+    {
+        if (collidedObj.gameObject.CompareTag("Street"))
+            jumpIsReady = true;
+        
+        else if (collidedObj.gameObject.CompareTag("Mover") || collidedObj.gameObject.CompareTag("DoubleMover"))
+        {
+            AddReward(-1.0f);
+            Debug.Log(GetCumulativeReward());
+            EndEpisode();
+        }
+    }
+
+    private void OnTriggerEnter(Collider collidedObj)
+    {
+        if (collidedObj.gameObject.CompareTag("score"))
+        {
+            AddReward(0.1f);
+            Debug.Log(GetCumulativeReward());
+            score++;
+            ScoreCollector.Instance.AddScore(score);
+        }
+    }
+}
